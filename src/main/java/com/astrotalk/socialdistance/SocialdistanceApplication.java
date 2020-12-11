86402b6f97ae304da77c6d5038ed4e6050a74761
Ayushman Srivastava <ayushman1024@gmail.com>
2020-12-11 22:48:37 +0530

    Initial commit

---
+++ src/main/java/com/astrotalk/socialdistance/SocialdistanceApplication.java
@@ -0,0 +1,13 @@
+package com.astrotalk.socialdistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialdistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialdistanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astrotalk/socialdistance/SocialdistanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astrotalk.socialdistance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialdistanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
