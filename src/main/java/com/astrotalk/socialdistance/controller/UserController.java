e2346d064cb4e19ce87c7b90ba9a008b78aba6cc
Ayushman Srivastava <ayushman1024@gmail.com>
2020-12-15 16:53:11 +0530

    corrected REST type

--- src/main/java/com/astrotalk/socialdistance/controller/UserController.java
+++ src/main/java/com/astrotalk/socialdistance/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,8 +37,8 @@ public class UserController {
 	    return new ResponseEntity<Boolean>(true,HttpStatus.OK);
 	  }
 
-	  @GetMapping("/removeFriend/{uid}/{fid}")
-	  public ResponseEntity<Boolean> removeFriend(@PathVariable("uid") long requester,@PathVariable("fid") long receiver) {
+	  @DeleteMapping("/removeFriend/{uid}/{fid}")
+	  public ResponseEntity<Boolean> removeFriend(@PathVariable("uid") long requester, @PathVariable("fid") long receiver) {
 	    userService.removeFriend(requester, receiver);
 	    return new ResponseEntity<Boolean>(true,HttpStatus.OK);
 	  }
